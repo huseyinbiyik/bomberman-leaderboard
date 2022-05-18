8332d3b494d2f1cc8b6f37c658bc865710affab8
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-05-18 17:28:22 +0300

    Add post data

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ refreshButton.onclick = () => {
 
 const displayBoard = document.querySelector(".display-board");
 let renderUi = (items) => {
-  displayBoard.innerHTML = '';
+  displayBoard.innerHTML = "";
   items.result.forEach(renderItems);
 };
 
@@ -28,15 +28,33 @@ let fetchData = () => {
 
 window.onload = fetchData();
 
-//   fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/FIHLzuAEnXayuCvu6FH3/scores', {
-//   method: 'POST',
-//   body: JSON.stringify({
-//     user: 'michael2',
-//     score: 55,
-//   }),
-//   headers: {
-//     'Content-type': 'application/json; charset=UTF-8',
-//   },
-// })
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
+const addScoreForm = document.querySelector("#add-score");
+const inputName = document.querySelector("#name");
+const inputScore = document.querySelector("#score");
+
+addScoreForm.onsubmit = (e) => {
+  e.preventDefault();
+  const name = inputName.value;
+  const score = inputScore.value;
+  postData(name, score);
+  addScoreForm.reset();
+};
+
+const postData = (name, score) => {
+
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/FIHLzuAEnXayuCvu6FH3/scores', {
+  method: 'POST',
+  body: JSON.stringify({
+    user: name,
+    score: score,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+};
+
+
