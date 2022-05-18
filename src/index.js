8db89fd49476c7ddbed56fb4de146eebd50d3169
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-05-18 16:12:36 +0300

    Add game id and basic fetch methods

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './style.css';
+
+// API "Game with ID: FIHLzuAEnXayuCvu6FH3
+
+fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/FIHLzuAEnXayuCvu6FH3/scores')
+  .then(response => response.json())
+  .then(json => console.log(json));
+
+//   fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/FIHLzuAEnXayuCvu6FH3/scores', {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     user: 'michael2',
+//     score: 55,
+//   }),    
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
