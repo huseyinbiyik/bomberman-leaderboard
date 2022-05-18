9ee151e4f531f7be42208809f086e611fbb54324
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-05-18 16:57:42 +0300

    Add refresh renderUi functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import './style.css';
+import "./style.css";
 
 // API "Game with ID: FIHLzuAEnXayuCvu6FH3
 
-fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/FIHLzuAEnXayuCvu6FH3/scores')
-  .then(response => response.json())
-  .then(json => console.log(json));
+const refreshButton = document.querySelector("#refresh-button");
+
+refreshButton.onclick = () => {
+  fetchData();
+};
+
+const displayBoard = document.querySelector(".display-board");
+let renderUi = (items) => {
+  displayBoard.innerHTML = '';
+  items.result.forEach(renderItems);
+};
+
+let renderItems = (item) => {
+  displayBoard.innerHTML += `<p>${item.user}: ${item.score}</p>`;
+};
+
+let fetchData = () => {
+  fetch(
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/FIHLzuAEnXayuCvu6FH3/scores"
+  )
+    .then((response) => response.json())
+    .then((json) => renderUi(json));
+};
+
+window.onload = fetchData();
 
 //   fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/FIHLzuAEnXayuCvu6FH3/scores', {
 //   method: 'POST',
 //   body: JSON.stringify({
 //     user: 'michael2',
 //     score: 55,
-//   }),    
+//   }),
 //   headers: {
 //     'Content-type': 'application/json; charset=UTF-8',
 //   },
