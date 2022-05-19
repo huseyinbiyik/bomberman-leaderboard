3505de149fb7212f037ad1169418c8240ec17e9f
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-05-19 11:50:51 +0300

    Change game token

--- src/modules/postData.js
+++ src/modules/postData.js
@@ -1,6 +1,6 @@
 const postData = async (name, score) => {
   await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/lQk6PtMS8WHtKZi99VDH/scores',
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/LzQ66iqiKtZlXDNStqI0/scores',
     {
       method: 'POST',
       body: JSON.stringify({
