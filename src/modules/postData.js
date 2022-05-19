5157f522cfe645625a91615f3de825bb144abb92
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-05-19 14:12:42 +0300

    Add active attribute to button

--- src/modules/postData.js
+++ src/modules/postData.js
@@ -1,17 +1,21 @@
+import fetchData from "./fetchData";
+
 const postData = async (name, score) => {
   await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/LzQ66iqiKtZlXDNStqI0/scores',
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/LzQ66iqiKtZlXDNStqI0/scores",
     {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
         user: name,
         score,
       }),
       headers: {
-        'Content-type': 'application/json; charset=UTF-8',
+        "Content-type": "application/json; charset=UTF-8",
       },
-    },
-  ).then((response) => response.json());
+    }
+  )
+    .then((response) => response.json())
+    .then(() => fetchData());
 };
 
 export default postData;
